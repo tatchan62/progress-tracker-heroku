f33c0eb4b1c5cf508ac54d6bb80971305fa92231
tatchan62 <tatchan62@gmail.com>
2020-04-30 18:55:29 -0500

    react static

---
+++ src/main/resources/static/precache-manifest.f7cc00b3db470c25ea82847685eb3293.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f695317c9beec6c6f936ae91095a8ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd2e72e125ba13e2d67b",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "3587be4e4dac2d935a31",
+    "url": "/static/css/main.f18cdaed.chunk.css"
+  },
+  {
+    "revision": "cd2e72e125ba13e2d67b",
+    "url": "/static/js/2.1486fe31.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.1486fe31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3587be4e4dac2d935a31",
+    "url": "/static/js/main.9f596a4f.chunk.js"
+  },
+  {
+    "revision": "700d31d1c6f37b7bdc51",
+    "url": "/static/js/runtime-main.84578d5e.js"
+  }
+]);
